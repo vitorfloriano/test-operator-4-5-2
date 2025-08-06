de6acef5fab86c9fd7219bd5abcd8cdc7f90a354
Vitor Floriano <107767584+vitorfloriano@users.noreply.github.com>
2025-08-06 19:30:58 -0300

    init commit with v4.5.2 and custom code

---
+++ internal/controller/frigate_controller.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	log "sigs.k8s.io/controller-runtime/pkg/log"
+
+	shipv1beta1 "github.com/vitorfloriano/test-operator-4-5-2/api/v1beta1"
+)
+
+// FrigateReconciler reconciles a Frigate object
+type FrigateReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=ship.my.domain,resources=frigates,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=ship.my.domain,resources=frigates/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=ship.my.domain,resources=frigates/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Frigate object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.4/pkg/reconcile
+func (r *FrigateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	frigate := &examplecomv1.Frigate{}
+	err := r.Get(ctx, req.NamespacedName, frigate)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("wordpress resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get wordpress")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("frigate reconciled")
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *FrigateReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&shipv1beta1.Frigate{}).
+		Named("frigate").
+		Complete(r)
+}
